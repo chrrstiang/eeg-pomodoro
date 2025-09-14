c3f4d5a2fc53ab2947c1b85ab70af19456a034be
chrrstiang <garcia.chris@northeastern.edu>
2025-09-14 00:41:32 -0400

    Reinstallation of expo project

--- frontend/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
-
-const config = getDefaultConfig(__dirname);
-
-// Set the app root for Expo Router
-process.env.EXPO_ROUTER_APP_ROOT = './app';
-
-module.exports = withNativeWind(config, { input: './global.css' });
